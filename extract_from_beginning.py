de723715c7381f74b7c53f08f9a5925a0c507171
Ian Kenney <ian.kenney@asu.edu>
2019-02-28 22:12:33 -0700

    initial commit

---
+++ extract_from_beginning.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import sys
+
+filename = sys.argv[1]
+print("Will write ids to {0}".format(filename))
+
+driver = webdriver.Firefox()
+
+driver.get("https://www.teamwork.com/launchpad/login?continue=/launchpad/welcome")
+driver.find_element(By.LINK_TEXT, "Single Sign On (SSO)").click()
+driver.find_element_by_id("loginemail").send_keys("pwberner@asu.edu")
+driver.find_element_by_xpath("/html/body/div/div[1]/section/div[2]/div/div/div/form/div[2]/button/span").click()
+
+raw_input("Press any key to continue")
+
+driver.get("https://clas.teamwork.com/#projects/568138/tasks")
+
+raw_input("Select task list and press enter")
+
+# load completed in general
+driver.find_element_by_xpath("/html/body/table/tbody/tr/td[2]/div[1]/div[1]/section/div[1]/div[1]/div/div[2]/a/span").click()
+
+# we want the thing
+base = "/html/body/table/tbody/tr/td[2]/div[1]/div[1]/section/div[1]/div[1]/div/div[4]/div[{0}]/div[1]/div[2]/p/a"
+
+xpath_by_index = lambda index : base.format(index)
+loadplease = lambda : driver.find_element_by_class_name('moreData').click()
+get_id = lambda elem : elem.get_attribute("href").split("/")[-1]
+
+completed_ids = []
+
+index = 2
+while True:
+    xpath = xpath_by_index(index)
+    try:
+        completed_ids.append(get_id(driver.find_element_by_xpath(xpath)))
+    except:
+        try:
+            loadplease()
+            completed_ids.append(get_id(driver.find_element_by_xpath(xpath)))
+        except:
+            break
+    index += 1 
+
+with open(filename, 'w') as f:
+    for elem in completed_ids:
+        f.write("{0}\n".format(elem))
---
+++ parse_tasks.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import pandas as pd
+import sys
+import time
+
+filename = sys.argv[1]
+
+ids = []
+
+with open(filename, 'r') as f:
+    for line in f:
+        ids.append(line.strip())
+
+driver = webdriver.Firefox()
+
+driver.get("https://www.teamwork.com/launchpad/login?continue=/launchpad/welcome")
+driver.find_element(By.LINK_TEXT, "Single Sign On (SSO)").click()
+driver.find_element_by_id("loginemail").send_keys("pwberner@asu.edu")
+driver.find_element_by_xpath("/html/body/div/div[1]/section/div[2]/div/div/div/form/div[2]/button/span").click()
+
+raw_input("press any key")
+
+taskurlbase = "https://clas.teamwork.com/#tasks/{0}"
+
+columns = ["category", "country", "state", "city", "varname", "comment"]
+df = pd.DataFrame(columns=columns)
+
+urlbyid      = lambda index : taskurlbase.format(index)
+_category    = lambda : driver.find_element_by_xpath("/html/body/table/tbody/tr/td[1]/div/div[2]/div[2]/div/div/p[1]/a").text
+_getlocation = lambda : tuple([i.strip() for i in driver.find_element_by_xpath("/html/body/table/tbody/tr/td[1]/div/div[2]/div[2]/div/div/p[2]/a").text.split(">")])
+_varname     = lambda : driver.find_element_by_xpath("/html/body/table/tbody/tr/td[2]/div[1]/div[1]/section/div[1]/div[1]/div/div[1]/div/div/div[1]/div[2]/p/span").text.split("-")[-1].strip()
+_comment     = lambda : driver.find_element_by_id("taskComments").text
+
+for i in ids[:10]:
+    url = urlbyid(i)
+    driver.get(url)
+    time.sleep(3)
+    try:
+        category = _category()
+        if not driver.find_element_by_xpath("/html/body/table/tbody/tr/td[1]/div/div[2]/div[2]/div/div/h3[2]").text == u'Parent Task':
+            continue
+    except:
+        time.sleep(5)
+        driver.find_element_by_id("sidebarToggle").click()
+    
+    if not driver.find_element_by_xpath("/html/body/table/tbody/tr/td[1]/div/div[2]/div[2]/div/div/h3[2]").text == u'Parent Task':
+        continue
+    category = _category()
+    country, state, city = _getlocation()
+    varname = _varname()
+    comment = _comment()
+    df = df.append(pd.DataFrame([[category, country, state, city, varname, comment]], columns=columns))
+
+df.to_csv(filename.replace(".txt", ".csv"),sep="|", encoding='utf-8',index=False)
